30545b332d68f88391a43be8fe090a93e7f4e63f
olimac62 <olimac62@hotmail.com>
2021-09-07 14:29:52 -0300

    nuevas actividaes

--- src/core/services/activities.js
+++ src/core/services/activities.js
@@ -1,24 +1,42 @@
 export const monthlySeminars = [
   {
     id: 1,
-    pdfPresentation: "/pdfs/activities/Laflechadeltiempoencosmología.pdf",
-    title: "La Flecha del Tiempo en Cosmología",
+    pdfPresentation: "/pdfs/activities/2021-03 Wandering thoughts on symmetries",
+    title: "Wandering thoughts on symmetries",
     description:
-      "En cosmología, suele denominarse “flecha cosmológica del tiempo” al sentido temporal de expansión del universo: dicha flecha será global en un universo Big Bang-Big Chill, pero apuntará en distintos sentidos en un universo Big Bang-Big Crunch. Esta terminología privilegia el “radio” del universo frente a otras propiedades geométricas del espacio-tiempo. Pero, ¿por qué aceptar el carácter privilegiado del radio respecto de otras propiedades geométricas del universo? En el presente trabajo se argumentará que: (i) La flecha del tiempo cosmológica no es una consecuencia inevitable de las leyes físicas: es una propiedad contingente del espacio-tiempo, (ii) los modelos cosmológicos standard de la cosmología contemporánea cumplen las condiciones necesarias para posee una flecha cosmológica del tiempo con alta probabilidad, y (iii) el carácter contingente de la flecha cosmológica del tiempo no minimiza su carácter objetivo.",
-    directedBy: "Seminar dictated by Olimpia Lombardi",
-    date: "7 de Abril de 2021",
+      "En el presente trabajo analizaré algunos aspectos de las simetrías en física. En particular, argumentaré que la interpretación de las simetrías en física depende fuertemente de: (i) qué se entiende por teoría física y ley, (ii) qué papel cumple la idealización en física y en metafísica, y (iii) que ontología subyacente se presupone.",
+    directedBy: "Seminar dictated by Cristian Lopez",
+    date: "17 de Marzo de 2021",
   },
   {
     id: 2,
-    pdfPresentation: "/pdfs/activities/Ideasonsymmetries.pdf",
-    title: "Ideas on symmetries",
+    pdfPresentation: "/pdfs/activities/2021-04 IMH y naturaleza relacional del tiempo.pdf",
+    title: "La interpretación modal-Hamiltoniana y la naturaleza relacional del tiempo",
     description:
-      "En cosmología, suele denominarse “flecha cosmológica del tiempo” al sentido temporal de expansión del universo: dicha flecha será global en un universo Big Bang-Big Chill, pero apuntará en distintos sentidos en un universo Big Bang-Big Crunch. Esta terminología privilegia el “radio” del universo frente a otras propiedades geométricas del espacio-tiempo. Pero, ¿por qué aceptar el carácter privilegiado del radio respecto de otras propiedades geométricas del universo? En el presente trabajo se argumentará que: (i) La flecha del tiempo cosmológica no es una consecuencia inevitable de las leyes físicas: es una propiedad contingente del espacio-tiempo, (ii) los modelos cosmológicos standard de la cosmología contemporánea cumplen las condiciones necesarias para posee una flecha cosmológica del tiempo con alta probabilidad, y (iii) el carácter contingente de la flecha cosmológica del tiempo no minimiza su carácter objetivo.",
-    directedBy: "Seminar dictated by Olimpia Lombardi",
-    date: "25 de Mayo de 2021",
+      "Sobre la base de la distinción entre tiempo externo y tiempo de eventos en mecánica cuántica, se propone una reconstrucción relacional del tiempo de eventos sobre la base de la interpretación modal-Hamiltoniana. Se señalará, además, que si se adopta una perspectiva holista que no distinga, el sistema completo resulta atemporal: no hay secuencia de eventos.",
+    directedBy: "Seminar dictated by Matías Pasqualini and Sebastian Fortin",
+    date: "14 de Abril de 2021",
   },
   {
     id: 3,
+    pdfPresentation: "/pdfs/activities/2021-05 Revisión artículo de De Haro",
+    title: "Revisión del artículo de De Haro ‘Noether’s Theorems and Energy in General Relativity’",
+    description:
+      "La presentación se abocará a revisar y comentar el artículo de Sebastian De Haro “Noether’s Theorems and Energy in General Relativity” (2021). En particular, se considerará el modo en que De Haro evalúa diferentes propuestas respecto de (i) la definición de energía (momentum) gravitacional y (ii) la conservación de la energía en relatividad general.",
+    directedBy: "Seminar dictated by Manuel Herrera and Cecilia Bejarano",
+    date: "19 de Mayo de 2021",
+  },
+  {
+    id: 4,
+    pdfPresentation: "/pdfs/activities/2021-06 Flecha global geométrica-revisando requisitos.pdf",
+    title: "El enfoque global-geométrico de la flecha del tiempo: revisando sus requisitos",
+    description:
+      "El problema de la flecha del tiempo consiste en brindar un fundamento físico a la diferencia entre las dos direcciones temporales. La respuesta tradicional apela al segundo principio de la termodinámica: la dirección pasado-a-futuro se concibe como la dirección en la que aumenta la entropía. El propósito de esta charla será presentar un enfoque diferente, según el cual la flecha del tiempo puede definirse en términos de las propiedades geométricas del espacio-tiempo, independientemente de argumentos termodinámicos. Desde esta perspectiva, la flecha del tiempo es una característica contingente que el universo posee si su espacio-tiempo cumple con los siguientes requisitos: orientabilidad temporal, existencia de tiempo global y asimetría temporal. A su vez, se analizarán las consecuencias de relajar cada uno de esos requisitos.",
+    directedBy: "Seminar dictated by Olimpia Lombardi and Cecilia Bejarano",
+    date: "16 de Junio de 2021",
+  },
+  {
+    id: 4,
     pdfPresentation: "/pdfs/activities/TiempoenMHI.pdf",
     title: "Tiempo en MHI",
     description:
